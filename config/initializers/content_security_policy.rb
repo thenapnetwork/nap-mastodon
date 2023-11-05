dc1edb48f54159eac183c48878e368803cc87159
000hen <git@muisnowdevs.one>
2023-11-05 11:13:42 +0800

    feat: Add cloudflare to Content Security Policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -58,7 +58,7 @@ Rails.application.config.content_security_policy do |p|
     p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host
   else
     p.connect_src :self, :data, :blob, assets_host, media_host, Rails.configuration.x.streaming_api_base_url
-    p.script_src  :self, assets_host, "'wasm-unsafe-eval'"
+    p.script_src  :self, assets_host, "https://static.cloudflareinsights.com", "'wasm-unsafe-eval'"
   end
 end
 
